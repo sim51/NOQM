fdd64929d9702da7fb5b0b4079c8b12f92066f6d
Benoît Simard (@logisima) <contact@bsimard.com>
2017-07-03 08:12:23 +0200

    Adding java doc

--- src/main/java/org/neo4j/driver/projection/Projection.java
+++ src/main/java/org/neo4j/driver/projection/Projection.java
@@ -1,103 +1,40 @@
 package org.neo4j.driver.projection;
 
+import org.neo4j.driver.exception.Neo4jClientException;
 import org.neo4j.driver.v1.Record;
-import org.neo4j.driver.v1.Value;
-import org.neo4j.driver.v1.types.Node;
-import org.neo4j.driver.v1.types.Path;
-import org.neo4j.driver.v1.types.Relationship;
 
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.function.Function;
 
+/**
+ * Abstract class for projection.
+ * A projection is just a function that can be passed to a map.
+ */
 abstract class Projection<T> implements Function<Record, T> {
 
-    protected static final Set<String> PRIMITIVE_TYPE = new HashSet<String>(
-            Arrays.asList(
-                    String.class.getSimpleName(),
-                    Long.class.getSimpleName(),
-                    Integer.class.getSimpleName(),
-                    Float.class.getSimpleName(),
-                    Boolean.class.getSimpleName(),
-                    Number.class.getSimpleName(),
-                    Double.class.getSimpleName(),
-                    Node.class.getSimpleName(),
-                    Relationship.class.getSimpleName(),
-                    Path.class.getSimpleName(),
-                    List.class.getSimpleName()
-            )
-    );
-
+    /**
+     * Projection's type.
+     */
     protected Class type;
 
+    /**
+     * Default constructor.
+     *
+     * @param type
+     */
     public Projection(Class type) {
         this.type = type;
     }
 
-    protected <Z>Z convertDriverValue(Class<Z> clazz, Value value) {
-        Z result = null;
-
-        // Depends of the projection
-        switch (clazz.getSimpleName()) {
-            case "String":
-                if (value != null) {
-                    result = (Z) value.toString();
-                }
-                break;
-            case "Long":
-                if (value != null) {
-                    result = (Z) Long.valueOf(value.asLong());
-                }
-                break;
-            case "Integer":
-                if (value != null) {
-                    result = (Z) Integer.valueOf(value.asInt());
-                }
-                break;
-            case "Float":
-                if (value != null) {
-                    result = (Z) Float.valueOf(value.asFloat());
-                }
-                break;
-            case "Boolean":
-                if (value != null) {
-                    result = (Z) Boolean.valueOf(value.asBoolean());
-                }
-                break;
-            case "Number":
-                if (value != null) {
-                    result = (Z) value.asNumber();
-                }
-                break;
-            case "Double":
-                if (value != null) {
-                    result = (Z) Double.valueOf(value.asDouble());
-                }
-                break;
-            case "Node":
-                if (value != null) {
-                    result = (Z) value.asNode();
-                }
-                break;
-            case "Relationship":
-                if (value != null) {
-                    result = (Z) value.asRelationship();
-                }
-                break;
-            case "Path":
-                if (value != null) {
-                    result = (Z) value.asPath();
-                }
-                break;
-            case "List":
-                if(value != null) {
-                    result = (Z) value.asList();
-                }
+    /**
+     * Check if the given <code>record</code> has only one column.
+     * if not, a {@link Neo4jClientException} is throw.
+     *
+     * @param record
+     */
+    protected void checkIfRecordHaveSingleValue(Record record) {
+        if (record.size() > 1) {
+            throw new Neo4jClientException("Record doesn't have a single column -> can't cast it to primitive object type");
         }
-        return result;
     }
 
 }
