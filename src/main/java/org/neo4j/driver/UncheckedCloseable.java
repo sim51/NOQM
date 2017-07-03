9c68d58bd07be230bb28edc6e85b74e4f3c3ddce
Benoît Simard (@logisima) <contact@bsimard.com>
2017-07-03 09:07:19 +0200

    Adding java doc and correct spelling

--- src/main/java/org/neo4j/driver/UncheckedCloseable.java
+++ src/main/java/org/neo4j/driver/UncheckedCloseable.java
@@ -1,24 +1,27 @@
 package org.neo4j.driver;
 
+import org.neo4j.driver.exception.Neo4jClientException;
+
+/**
+ * Wrap an AutoCloseable object for adding the Runnable trait.
+ */
 public interface UncheckedCloseable extends Runnable, AutoCloseable {
 
+    /**
+     * Wrap the defined AutoCloseable object.
+     */
+    static UncheckedCloseable wrap(AutoCloseable c) {
+        return c::close;
+    }
+
+    /**
+     * Default implementation of {@link Runnable#run}.
+     */
     default void run() {
         try {
             close();
         } catch (Exception e) {
             throw new Neo4jClientException(e);
         }
-    }
-
-    default UncheckedCloseable nest(AutoCloseable c) {
-        return () -> {
-            try (UncheckedCloseable c1 = this) {
-                c.close();
-            }
-        };
-    }
-
-    static UncheckedCloseable wrap(AutoCloseable c) {
-        return c::close;
     }
 }
